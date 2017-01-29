c7207ed5920c3e41ef3cdcd6bee7e06027112756
Juan Diego De Miguel Mateo <johndi9@hotmail.com>
2017-01-29 09:46:00 +0100

    Typing fix

---
+++ src/app/components/panels/home/cv-container/other-info-wrapper/other-info-wrapper.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'other-info-wrapper',
+  styleUrls: ['./other-info-wrapper.scss'],
+  templateUrl: './other-info-wrapper.html'
+})
+
+export class OtherInfoWrapper {
+  constructor() {
+  }
+}

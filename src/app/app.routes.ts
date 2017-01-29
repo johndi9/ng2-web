cd78d15c69a233f573a8da2f133b03dab703d032
Juan Diego De Miguel Mateo <johndi9@hotmail.com>
2017-01-29 09:46:17 +0100

    Removed route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { DataResolver } from './app.resolver';
 import { Home } from './components/pages/home/home.component';
 import { NoContent } from './components/pages/no-content/no-content.component';
-import { WhoIAm } from './components/pages/who-i-am/who-i-am.component';
 
 
 export const ROUTES: Routes = [
   {path: '', component: Home},
   {path: 'home', component: Home},
-  {path: 'whoIAm', component: WhoIAm},
   {path: '**', component: NoContent},
 ];

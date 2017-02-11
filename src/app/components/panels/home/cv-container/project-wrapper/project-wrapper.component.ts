36758ac85fcd0e38be994e38b49554cc6eb414b1
Juan Diego De Miguel Mateo <johndi9@hotmail.com>
2017-02-11 12:39:40 +0100

    - First new layout for projects

--- src/app/components/panels/home/cv-container/project-wrapper/project-wrapper.component.ts
+++ src/app/components/panels/home/cv-container/project-wrapper/project-wrapper.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy, AfterViewInit } from '@angular/core';
+import { MdDialog, MdDialogConfig } from '@angular/material';
+
+import { Project } from '../../../../../models/Curriculum/Project/Project';
+import { ProjectDialog } from '../../dialogs/project-dialog/project-dialog.component';
 
 
 @Component({
   selector: 'project-wrapper',
   styleUrls: ['./project-wrapper.scss'],
-  templateUrl: './project-wrapper.html'
+  templateUrl: './project-wrapper.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class ProjectWrapper {
-  constructor() {
+export class ProjectWrapper implements AfterViewInit{
+  @Input() projects: Project[];
+
+  config: MdDialogConfig = {
+    disableClose: false,
+    width: '',
+    height: '',
+    position: {
+      top: '',
+      bottom: '',
+      left: '',
+      right: ''
+    }
+  };
+  private isLoadingView: boolean = true;
+
+  constructor(public dialog: MdDialog) {
+  }
+
+  ngAfterViewInit(): void {
+    this.isLoadingView = false;
+  }
+
+  private openModal(project: Project): void {
+    let dialogRef = this.dialog.open(ProjectDialog, this.config);
+    dialogRef.componentInstance.project = project;
   }
 }

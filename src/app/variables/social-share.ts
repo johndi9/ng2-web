941518453876830c4d2e2fe1e3bfcc2c348fa4af
Juan Diego De Miguel Mateo <johndi9@hotmail.com>
2017-04-11 22:47:07 +0200

    - Added LinkedIn share button

--- src/app/variables/social-share.ts
+++ src/app/variables/social-share.ts
@@ -1,8 +1,10 @@
 const website: string = 'http://jdmiguel.xyz';
+const title: string = 'Juan Diego de Miguel Website';
+const summary: string = 'Check my new website with Angular2 / Google Material';
 
 export const SOCIAL_SHARE = {
-  twitter: 'https://twitter.com/intent/tweet?text=' +
-  encodeURI(`Check my new website with Angular2 / Google Material\n${website} via @juandiegodemi'`),
-  googlePlus: `https://plus.google.com/share?url=${website}`,
-  facebook: `https://www.facebook.com/dialog/share?app_id=429283144074151&display=popup&href=${website}`
+  twitter: encodeURI(`https://twitter.com/intent/tweet?text=${summary}&via=juandiegodemi&url=${website}`),
+  googlePlus: encodeURI(`https://plus.google.com/share?url=${website}`),
+  facebook: encodeURI(`https://www.facebook.com/dialog/share?app_id=429283144074151&display=popup&href=${website}&quote=${summary}`),
+  linkedIn: encodeURI(`https://www.linkedin.com/shareArticle?url=${website}&title=${title}&summary=${summary}&source=${website}`)
 };

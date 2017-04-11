16bb5741da385d7f9e07589eaffbaef718738cf8
Juan Diego De Miguel Mateo <johndi9@hotmail.com>
2017-04-11 21:24:12 +0200

    - Added Google + share

--- src/app/variables/social-share.ts
+++ src/app/variables/social-share.ts
@@ -1,4 +1,8 @@
+const website: string = 'http://jdmiguel.xyz';
+
 export const SOCIAL_SHARE = {
   twitter: 'https://twitter.com/intent/tweet?text=' +
   encodeURI(`Check my new website with Angular2 / Google Material\n${website} via @juandiegodemi'`),
+
+  googlePlus: `https://plus.google.com/share?url=${website}`
 };

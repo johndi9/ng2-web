cb14914c83c4a87bf515a655567219d6cc3ab789
Juan Diego De Miguel Mateo <johndi9@hotmail.com>
2017-06-10 00:37:20 +0100

    - New routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,16 @@ import { DataResolver } from './app.resolver';
 import { Home } from './components/pages/home/home.component';
 import { NoContent } from './components/pages/no-content/no-content.component';
 
-
 export const ROUTES: Routes = [
   { path: '', component: Home },
-  { path: 'home', component: Home },
+  { path: 'personal', component: Home },
+  { path: 'projects', component: Home },
+  { path: 'projects/:id', component: Home },
+  { path: 'employers', component: Home },
+  { path: 'employers/:id', component: Home },
+  { path: 'education', component: Home },
+  { path: 'other', component: Home },
+  { path: 'contact', component: Home },
   { path: '**', component: NoContent },
   // { path: 'detail', loadChildren: './+detail#DetailModule'},
 ];
